7113f03686e3368ef8872ccee5933ec3bd45e8de
mackliu <mackliu@gmail.com>
2022-07-28 16:20:20 +0800

    完成在右上顯示登入者帳號及管理者管理功能按鈕

--- index.php
+++ index.php
@@ -37,8 +37,28 @@
 請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章
 				</marquee>
                 	<span style="width:18%; display:inline-block;">
-                    	                    	<a href="?do=login">會員登入</a>
-                    	                    </span>
+							<?php
+							if(isset($_SESSION['user'])){
+								if($_SESSION['user']==='admin'){
+								?>
+								歡迎，<?=$_SESSION['user'];?>
+								<button onclick="location.href='back.php'">管理</button>
+								|<button onclick='logout()'>登出</button>
+								<?php
+								}else{
+							?>
+							歡迎，<?=$_SESSION['user'];?>
+							<button onclick='logout()'>登出</button>
+							<?php
+							}
+							}else{
+							?>
+							<a href="?do=login">會員登入</a>
+							<?php	
+							}
+							?>
+                    	    
+                    	    </span>
                     	<div class="content">
 							<?php 
 
