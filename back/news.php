47f913d577dfcd4bfc31672f54cd4538a58c027f
mackliu <mackliu@gmail.com>
2022-07-29 15:22:01 +0800

    完成文章管理的分頁功能

--- back/news.php
+++ back/news.php
@@ -1,5 +1,5 @@
 <form action="./api/news.php" method="post">
-<table class="tab cent ct">
+<table style="width:80%;margin:auto;text-align:center">
     <tr>
         <th width="10%">編號</th>
         <th width="70%">標題</th>
@@ -7,7 +7,13 @@
         <th width="10%">刪除</th>
     </tr>
     <?php
-    $rows=$News->all();
+    $all=$News->math('count','id');
+    $div=3;
+    $pages=ceil($all/$div);
+    $now=$_GET['p']??1;
+    $start=($now-1)*$div;
+    $rows=$News->all(" limit $start ,$div");
+
     foreach($rows as $key => $row){
 
     ?>
@@ -24,6 +30,28 @@
     ?>
 
 </table>
+<div class="ct">
+    <?php
+    if(($now-1)>0){
+        echo "<a href='?do=news&p=".
+              ($now-1).
+             "'> < </a>";
+    }
 
+    for($i=1;$i<=$pages; $i++){
+        $fontsize=($now==$i)?'24px':'16px';
+
+        echo "<a href='?do=news&p={$i}' style='font-size:$fontsize'> ";
+        echo $i;
+        echo " </a>";
+    }
+    if(($now+1)<=$pages){
+        echo "<a href='?do=news&p=".
+              ($now+1).
+             "'> > </a>";
+    }
+
+    ?>
+</div>
 <div class="ct"><input type="submit" value="確定修改"></div>
 </form>
