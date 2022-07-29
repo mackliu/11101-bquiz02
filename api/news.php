623481bd22eeb30ccc6013b43e0e4b36fa35abf0
mackliu <mackliu@gmail.com>
2022-07-29 14:24:06 +0800

    完成文章管理的顯示及刪除功能

---
+++ api/news.php
@@ -0,0 +1,22 @@
+<?php
+include_once "../base.php";
+
+if(isset($_POST['del'])){
+    foreach($_POST['del'] as $id){
+        $News->del($id);
+    }
+}
+
+$rows=$News->all();
+
+foreach($rows as $row){
+    if(in_array($row['id'],$_POST['sh'])){
+        $row['sh']=1;
+    }else{
+        $row['sh']=0;
+
+    }
+    $News->save($row);
+}
+
+to("../back.php?do=news");
