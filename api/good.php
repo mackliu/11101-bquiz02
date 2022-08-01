c29c1a24aadb4d952df070e31d410917d184ba8e
mackliu <mackliu@gmail.com>
2022-08-01 14:52:47 +0800

    增加人氣按讚文章讚數加1

---
+++ api/good.php
@@ -0,0 +1,20 @@
+<?php
+include_once "../base.php";
+$type=$_POST['type'];
+$id=$_POST['id'];
+
+$news=$News->find($id);
+switch($type){
+    case '讚':
+
+    $news['good']++;
+
+    break;
+    case '收回讚':
+    $news['good']--;
+    break;
+}
+
+$News->save($news);
+
+?>
