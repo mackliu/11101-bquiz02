fce4d9aa9f47524663c8fa53b6056c9f920d72bf
mackliu <mackliu@gmail.com>
2022-08-01 09:45:26 +0800

    建立分類網誌基本外觀及點擊功能

--- front/po.php
+++ front/po.php
@@ -1,1 +1,35 @@
-分類網誌
+<style>
+    .type{
+        cursor: pointer;
+        color:blue;
+        margin:1rem 0;
+        max-width: max-content;
+    }
+    .type:hover{
+        border-bottom:1px solid blue;
+    }
+</style>
+<div>目前位置：首頁 > 分類網誌 > <span id="header">健康新知</span></div>
+<div style="display:flex">
+    <fieldset>
+        <legend>分類網誌</legend>
+        <div class="type">健康新知</div>
+        <div class="type">菸害防制</div>
+        <div class="type">癌症防治</div>
+        <div class="type">慢性病防治</div>
+    </fieldset>
+    <fieldset>
+        <legend>文章列表</legend>
+        <div></div>
+    </fieldset>
+</div>
+
+<script>
+
+$(".type").on("click",function(){
+    let type=$(this).text()
+    $("#header").text(type)
+})
+
+
+</script>
