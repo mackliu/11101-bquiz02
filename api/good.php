41336a0702660233be590636fdcd25da00144848
mackliu <mackliu@gmail.com>
2022-08-01 15:24:16 +0800

    完成人氣文章按讚收回讚功能

--- api/good.php
+++ api/good.php
@@ -6,12 +6,13 @@ $id=$_POST['id'];
 $news=$News->find($id);
 switch($type){
     case '讚':
-
     $news['good']++;
+    $Log->save(['news'=>$id,'user'=>$_SESSION['user']]);
 
     break;
     case '收回讚':
     $news['good']--;
+    $Log->del(['news'=>$id,'user'=>$_SESSION['user']]);
     break;
 }
 
